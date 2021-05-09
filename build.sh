6c81fd21b9ae9f2df8179436af05443a5df5e2a5
aeryz <abdullaheryz@protonmail.com>
2021-05-10 01:28:05 +0300

    POST: simple-kernel

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@
 i386-elf-as -msyntax=intel -mmnemonic=intel boot.s -o boot.o
 i386-elf-gcc -c vga.c -ffreestanding -o vga.o
 i386-elf-ld -T linker.ld boot.o vga.o -o kernel.bin 
-
-#i386-elf-gcc -T linker.ld -o kernel.bin --freestanding -O2 -nostdlib boot.o vga.o -lgcc
-
